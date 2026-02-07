d1390fdf83c719c4b47d8db65c2dffbe8c0ef5a4
Hussein-shsx3 <husseinshsx3@gmail.com>
2026-02-07 11:38:27 +0200

    edit

--- client/src/components/ui/buttons/index.ts
+++ client/src/components/ui/buttons/index.ts
@@ -1,4 +1,3 @@
-export { AddButton } from "./AddButton";
 export { LogoutButton } from "./LogoutButton";
 export { default as GoogleButton } from "./GoogleButton";
 export { ThemeToggle } from "./ThemeToggle";
